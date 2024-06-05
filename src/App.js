055642022cace84bf898a73ffbf3925292a8fa83
ruslan81 <ruslan.dev@gmail.com>
2024-06-06 01:20:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Welcome to my react application, {value}</div>;
 }
 
 export default App;
